7d1c02a36d913e79a60c1eed38034d796b35a367
Gabriel-hrq <gabriel.hrq@ufu.br>
2024-06-26 16:52:59 -0300

    Inclusão da classe Message

---
+++ rede-social/source/Message.py
@@ -0,0 +1,7 @@
+class Message:
+    def __init__(self,id_message,content,sender,reciver,time):
+        self.id_message = id_message
+        self.content = content
+        self.sender = sender
+        self.reciver = reciver
+        self.time = time
