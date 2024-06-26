2f663a9b531f51bda53b842ca6eb905663d579cd
Gabriel-hrq <gabriel.hrq@ufu.br>
2024-06-26 16:50:10 -0300

    Alteração de variáveis e inclusão de email

--- rede-social/source/User.py
+++ rede-social/source/User.py
@@ -1,8 +1,11 @@
 class User:
-    def __init__(self,id,name,received,count_received,sent,count_sent):
-        self.id = id
+    def __init__(self,id_user,name,received,count_received,sent,count_sent,email):
+        self.id_user = id_user
         self.name = name
         self.received = []
         self.count_received = 0
         self.sent = []
         self.count_sent = 0
+        self.email = email
+
+
